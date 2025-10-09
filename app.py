9485d98134d1702a5e4434e6cdf0ae751237dd3e
Annemarie Sodmann <49727849+AmSchulte@users.noreply.github.com>
2025-10-09 12:22:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def normalize_to_uint8(image, bit_depth=12):
 @st.cache_data
 def load_metadata():
     # Load the metadata file
-    metadata = pd.read_excel("https://raw.githubusercontent.com/AmSchulte/DRGrat/main/data/metadata_filtered.xlsx")
+    metadata = pd.read_excel("https://raw.githubusercontent.com/AmSchulte/DRGrat/main/metadata_filtered.xlsx")
     return metadata
 # Load the metadata
 metadata_filtered = load_metadata()
